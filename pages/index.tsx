da28774cc5969a8b48cb4ef547bdcd463edff3fa
Udom Kasemosoth <udom.kas@1moby.com>
2022-07-20 10:28:30 +0700

    update home loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { Space, Spin } from 'antd';
 import { MainLayout } from '../components/layout';
 
 const HomePage: NextPage = () => {
@@ -10,10 +11,9 @@ const HomePage: NextPage = () => {
   }, []);
 
   return (
-    <MainLayout>
-      <h1>Welcome</h1>
-      <p>main page</p>
-    </MainLayout>
+    <div style={{ height: '100vh', padding: '50vh 50vw' }}>
+      <Spin size="large" />
+    </div>
   );
 };
 
